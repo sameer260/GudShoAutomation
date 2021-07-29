322faf54962a3a6f6fbd884ac1e4dea41c750711
SameerSK_Contus <sameer.g@contus.in>
2021-07-29 16:14:22 +0530

    runner

--- WebAutomation/src/main/java/Pageobjects/frontend/accountandsettingspage.java
+++ WebAutomation/src/main/java/Pageobjects/frontend/accountandsettingspage.java
@@ -21,7 +21,4 @@ public class accountandsettingspage extends BaseSetup
 	
 	@FindBy(xpath="//div[@class='notify-wrapper']")
 	public static WebElement NotificationElements;
-	
-	@FindBy(xpath="//div[@class='notify-wrapper']")
-	public static WebElement NotificationElements1;
 }
