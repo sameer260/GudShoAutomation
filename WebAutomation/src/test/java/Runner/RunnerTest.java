008eb29e3646074743dd30536265522c79e13e1a
SameerSK_Contus <sameer.g@contus.in>
2021-07-29 13:02:38 +0530

    u

--- WebAutomation/src/test/java/Runner/RunnerTest.java
+++ WebAutomation/src/test/java/Runner/RunnerTest.java
@@ -23,7 +23,6 @@ import net.masterthought.cucumber.sorting.SortingMethod;
 	@RunWith(Cucumber.class)
 	@CucumberOptions(features= "src/test/resources/Features/frontend",
 	glue={"steps"},
-	tags="@Paywall",
 	plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 			  "pretty","json:target/cucumber-reports/Cucumber.json", 
 			  "junit:target/cucumber-reports/Cucumber.xml",
